69c8f33bfdd9011af698c04de34040b3f1fb20cd
Yusuf-Sokunbi <adeloaoo87@gmail.com>
2024-10-14 14:31:51 +0100

    Starting With Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogoImg from '../assets/logo.jpg'
+export default function Header() {
+  return (<header id="main-header">
+    <div id="title">
+      <img src={LogoImg} alt='A restaurant'/>
+      <h1>Food Restaurant</h1>
+    </div>
+    <nav>
+    <button>Cart(0)</button>
+    </nav>
+    </header>
+  )
+}
